e2e4d5ba892c493d56f5c268ee38c0acdba9e0fc
Thomas de Visser <thomasdevisser@pm.me>
2023-01-07 22:27:56 +0100

    Finished standard I/O section

--- standard-io.sh
+++ standard-io.sh
@@ -10,3 +10,12 @@ echo ${ARGS[0]}
 # This is how you print all arguments
 ARGS="$@"
 echo "$ARGS"
+
+# 1> represents stdout and 2> stderr
+# > also represents stdout
+lss -la 2>error.txt
+ls 1>ls.txt
+
+# These ways the file is used for both output and errors
+ls > file.txt 2>&1
+ls >& file.txt
